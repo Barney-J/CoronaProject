14baacd4fb1921049ef705da4edd119e274dfaaa
Barney_J <barney.jackson50@gmail.com>
2020-10-31 17:50:37 +0300

    Initial Commit

---
+++ CoronaProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoronaProject1
+//
+//  Created by Eugene Sushko on 10/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

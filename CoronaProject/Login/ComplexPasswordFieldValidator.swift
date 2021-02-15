3bf58127d58371d6567652ff0d5ac869cafc7103
Barney <barney.jackson50@gmail.com>
2021-02-15 23:39:01 +0300

    Rebuild ComplexLogPassFieldValidator

--- CoronaProject/Login/ComplexPasswordFieldValidator.swift
+++ CoronaProject/Login/ComplexPasswordFieldValidator.swift
@@ -3,23 +3,36 @@ import Foundation
 
 class ComplexLogPassFieldValidator: FieldValidator {
     func checkLoginAndPassword (_ login: String ,_ password: String) -> Bool{
-        if ((login != "") && (checkPasswordCount(password))){
+        if ((login != "") && (validatePassword(password))){
             return true
         } else {
             return false
         }
         
     }
+    private func validatePassword(_ passwordText: String) -> Bool {
+            if passwordText.count >= 8 && differentCaseExisting(passwordText) {
+                return true
+            } else {
+                return false
+            }
+        }
     
-    private func checkPasswordCount(_ password: String) -> Bool{
-        if password.count >= 9 {
+    private func differentCaseExisting(_ str: String) -> Bool {
+        var upperCase = false
+        var lowerCase = false
+        for char in str {
+            if char.isUppercase {
+                 upperCase = true
+             }
+            if char.isLowercase {
+                lowerCase = true
+            }
+         }
+        if upperCase && lowerCase {
             return true
         } else {
             return false
         }
-    }
-    
-    func passwordValidator(_ password: String) -> Bool {
-        return false
-    }
+     }
 }

3113a160a60ecbb7e1a4117fe80e48de703db7f9
Barney <barney.jackson50@gmail.com>
2021-02-17 00:25:25 +0300

    Remane Dependecies

--- CoronaProject/AppDelegate.swift
+++ CoronaProject/AppDelegate.swift
@@ -19,9 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         notificationCenter.delegate = self
         sendNotifications()
-        Dependency.container = Container()
-        Dependency.registerDependency()
-        logger = Logger(eventManager: Dependency.container.resolve(EventManager.self)!)
+        Dependecies.container = Container()
+        Dependecies.registerDependecies()
+        logger = Logger(eventManager: Dependecies.container.resolve(EventManager.self)!)
         
         FirebaseApp.configure()
 
@@ -43,15 +43,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
 
--- CoronaProject/Dependecies.swift
+++ CoronaProject/Dependecies.swift
@@ -2,22 +2,17 @@ import Foundation
 import Swinject
 
 
-class Dependency {
+class Dependecies {
     static var container: Container!
     
-    static func registerDependency () {
+    static func registerDependecies () {
         container.register(FieldValidator.self) {_ in
             let complexLogPassFieldValidator = ComplexLogPassFieldValidator()
-            let passwordValidator = AttemptsCountValidator(fieldValidator: complexLogPassFieldValidator)
-            return passwordValidator}
+            let attemptsCountValidator = AttemptsCountValidator(fieldValidator: complexLogPassFieldValidator)
+            return attemptsCountValidator}
         container.register(StyleLoginVCManager.self, name: "Light") { _ in LightStyle()}
         container.register(StyleLoginVCManager.self, name: "Dark") { _ in DarkStyle()}
         container.register(ProtocolTimerControl.self) { _ in TimerControl()}
-        container.register(ModelView.self) {_ in
-            let complexLogPassFieldValidator = ComplexLogPassFieldValidator()
-            let loginModelView = LoginModelView(validator: complexLogPassFieldValidator)
-            return loginModelView
-        }
         container.register(EventManager.self) {_ in ListEventManager()}.inObjectScope(.container)
     }
 }
